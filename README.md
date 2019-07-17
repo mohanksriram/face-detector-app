1f614c795b5cb8cf16d49d1a30e25a77c16f692c
mohankumarSriram <thacoolsoul@gmail.com>
2019-07-17 16:16:42 +0530

    updated readme

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # face-detector-app
+
+## Steps
+
+Install Conda
+https://www.anaconda.com/distribution/
+
+Create a new environment and activate
+```
+conda create -n tf-keras python=3.7
+conda activate tf-keras
+```
+Clone the repo
+```
+git clone https://github.com/mohankumarSriram/face-verification.git
+```
+
+- Download data [here](https://drive.google.com/open?id=1vqecrTsYKU8BUPIfa8edXFWLPCveBdjH)
+- Download pre-trained models [here](https://drive.google.com/open?id=15nyPwd9bCZDG3D8wgz_d22jCR3AMVkUp)
+
+```
+unzip ~/Downloads/data.zip -d ./
+unzip ~/Downloads/model.zip -d ./
+```
+
+Install requiremnts
+```
+pip install -r requirements.txt
+```
+
+Launch App
+```
+python serve.py
+```
---
+++ build/precache-manifest.b888c5a2b3a2ee4ba736f4108fcfd7b2.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5062bcbcd300d891dda20bf1a81e513e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4b7b60ce4f6c1e3ec85e",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "4dfafae96e6e0669aad9",
+    "url": "/static/css/main.aadd5ce6.chunk.css"
+  },
+  {
+    "revision": "4b7b60ce4f6c1e3ec85e",
+    "url": "/static/js/2.611d35f5.chunk.js"
+  },
+  {
+    "revision": "4dfafae96e6e0669aad9",
+    "url": "/static/js/main.823f9355.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "8e9f0a3c5578a20733d5bad0e51c91fb",
+    "url": "/static/media/sidebar-1.8e9f0a3c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "2503169017014036cf0dd0dd7c2a2b8a",
+    "url": "/static/media/sidebar-3.25031690.jpg"
+  },
+  {
+    "revision": "fc9cb0538eb5a4dfd6fbb1bf6dd6189b",
+    "url": "/static/media/sidebar-4.fc9cb053.jpg"
+  }
+]);
